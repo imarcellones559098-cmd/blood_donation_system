167ef720cb0802189f015090abe80439f7186400
Ian Ray Marcellones <i.marcellones.559098@umindanao.edu.ph>
2026-05-22 09:19:56 -0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,16 +10,13 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-             'email' => 'admin@blood.com',
-    'password' => bcrypt('admin123'),
-        ]);
+        if (!User::where('email', 'admin@blood.com')->exists()) {
+            User::factory()->create([
+                'email' => 'admin@blood.com',
+                'password' => bcrypt('admin123'),
+            ]);
+        }
     }
 }
